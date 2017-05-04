700aefbe7a4f2eaadce3f24f87a9648686d87287
Andrey Mayorov <a.mayorov@rbkmoney.com>
2017-05-04 13:16:39 +0300

    HG-190: Switch to damsel upstream, bump to rbkmoney/machinegun@a5a8653

--- docker-compose.sh
+++ docker-compose.sh
@@ -24,7 +24,7 @@ services:
         condition: service_healthy
 
   machinegun:
-    image: dr.rbkmoney.com/rbkmoney/machinegun:e04e529f4c5682b527d12d73a13a3cf9eb296d4d
+    image: dr.rbkmoney.com/rbkmoney/machinegun:a5a86531799b421e003488803e278b91a20ec099
     command: /opt/machinegun/bin/machinegun foreground
     volumes:
       - ./test/machinegun/sys.config:/opt/machinegun/releases/0.1.0/sys.config
