ffe93d02f365acd353396a9a79811acff41e5c93
pulitta <pulitta11@gmail.com>
2017-09-14 19:36:29 +0300

    HG-274: renamed hg_event_sink* modules in correct way

--- apps/hellgate/src/hg_event_sink_client.erl
+++ apps/hellgate/src/hg_event_sink_client.erl
@@ -1,37 +1,53 @@
 -module(hg_event_sink_client).
 
--export([handle_function/3]).
+-export([get_events/2]).
+-export([get_last_event_id/0]).
 
-%% Woody handler called by hg_woody_wrapper
+-include_lib("mg_proto/include/mg_proto_state_processing_thrift.hrl").
 
--behaviour(hg_woody_wrapper).
 
-%%
+-type event() :: mg_proto_state_processing_thrift:'Event'().
+-type event_id() :: mg_proto_base_thrift:'ID'().
 
--include_lib("dmsl/include/dmsl_payment_processing_thrift.hrl").
+-spec get_events(event_id(), integer()) ->
+        [event()] | {error, event_not_found}.
 
--type event_id() :: dmsl_base_thrift:'EventID'().
--type event()    :: dmsl_payment_processing_thrift:'Event'().
-
--spec handle_function
-    ('GetEvents', woody:args(), hg_woody_wrapper:handler_opts()) ->
-        [event()] | no_return();
-    ('GetLastEventID', woody:args(), hg_woody_wrapper:handler_opts()) ->
-        event_id() | no_return().
-
-handle_function('GetEvents', [#payproc_EventRange{'after' = After, limit = Limit}], _Opts) ->
-    case hg_event_sink:get_events(After, Limit) of
-        {ok, Events} ->
-            Events;
-        {error, event_not_found} ->
-            throw(#payproc_EventNotFound{})
-    end;
-
-handle_function('GetLastEventID', [], _Opts) ->
-    % TODO handle thrift exceptions here
-    case hg_event_sink:get_last_event_id() of
-        {ok, ID} ->
-            ID;
-        {error, no_last_event} ->
-            throw(#payproc_NoLastEvent{})
+get_events(After, Limit) ->
+    try
+        get_public_history(After, Limit)
+    catch
+        {exception, #'EventNotFound'{}} ->
+            {error, event_not_found}
     end.
+
+-spec get_last_event_id() ->
+        event() | {error, no_last_event}.
+
+get_last_event_id() ->
+    case get_history_range(undefined, 1, backward) of
+        [#'SinkEvent'{id = ID}] ->
+            {ok, ID};
+        [] ->
+            {error, no_last_event}
+    end.
+
+get_public_history(After, Limit) ->
+    History = [publish_event(Ev) || Ev <- get_history_range(After, Limit)],
+    {ok, History}.
+
+get_history_range(After, Limit) ->
+    get_history_range(After, Limit, forward).
+
+get_history_range(After, Limit, Direction) ->
+    HistoryRange = #'HistoryRange'{'after' = After, limit = Limit, direction = Direction},
+    {ok, History} = call_event_sink('GetHistory', [HistoryRange]),
+    History.
+
+publish_event(#'SinkEvent'{id = ID, source_ns = Ns, source_id = SourceID, event = Event}) ->
+    #'Event'{id = EventID, created_at = Dt, event_payload = Payload} = Event,
+    hg_event_provider:publish_event(Ns, ID, SourceID,  {EventID, Dt, hg_msgpack_marshalling:unmarshal(Payload)}).
+
+-define(EVENTSINK_ID, <<"payproc">>).
+
+call_event_sink(Function, Args) ->
+    hg_woody_wrapper:call('EventSink', Function, [?EVENTSINK_ID | Args]).
