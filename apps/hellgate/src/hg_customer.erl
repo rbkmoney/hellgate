5955af69ea5cb8da4d95a253d8775190e1a703d8
Andrey Fadeev <me@ciiol.net>
2018-09-19 14:57:24 +0300

    HG-424 Increace hardcoded customer deadline

--- apps/hellgate/src/hg_customer.erl
+++ apps/hellgate/src/hg_customer.erl
@@ -551,7 +551,7 @@ is_binding_outdated(BindingId, #st{binding_starts = Starts}) ->
     % Old customers will failed after `hardcoded timestamp + ?MAX_BINDING_DURATION`
     BindingStart = case BindingStart0 of
         undefined ->
-            <<"2018-09-19T09:00:00Z">>;
+            <<"2018-09-20T10:00:00Z">>;
         _ ->
             BindingStart0
     end,
