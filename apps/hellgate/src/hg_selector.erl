ac2fde0696f8a529044ea36172246056aa23fd9c
Roman Pushkov <pushkovroman@me.com>
2019-05-28 16:22:09 +0300

    add terminal to varset

--- apps/hellgate/src/hg_selector.erl
+++ apps/hellgate/src/hg_selector.erl
@@ -32,6 +32,7 @@
 -type varset() :: #{
     category        => dmsl_domain_thrift:'CategoryRef'(),
     currency        => dmsl_domain_thrift:'CurrencyRef'(),
+    terminal        => dmsl_domain_thrift:'TerminalRef'(),
     cost            => dmsl_domain_thrift:'Cash'(),
     payment_tool    => dmsl_domain_thrift:'PaymentTool'(),
     party_id        => dmsl_domain_thrift:'PartyID'(),
