42a478fd032d24bd048af40f38878a0ca5d81eb0
Roman Pushkov <pushkovroman@me.com>
2019-05-28 16:26:08 +0300

    update dmsl and dominant

--- docker-compose.sh
+++ docker-compose.sh
@@ -17,7 +17,7 @@ services:
         condition: service_healthy
 
   dominant:
-    image: dr.rbkmoney.com/rbkmoney/dominant:5a2be39e1035bf590af2e2a638062d6964708e05
+    image: dr.rbkmoney.com/rbkmoney/dominant:a8ebe82fb305a5e3cc8fa49b9e963290c3244a55
     command: /opt/dominant/bin/dominant foreground
     depends_on:
       machinegun:
