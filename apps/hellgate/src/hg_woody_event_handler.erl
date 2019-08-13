168b7477ac13d08ebcba74b598a2c61f23d94267
Sergey Yelin <elinsn@gmail.com>
2019-08-13 17:46:11 +0300

    Added result formatter

--- apps/hellgate/src/hg_woody_event_handler.erl
+++ apps/hellgate/src/hg_woody_event_handler.erl
@@ -23,14 +23,10 @@
 
 handle_event(Event, RpcID, RawMeta, Opts) ->
     FormattedMeta = format_meta(RawMeta),
-    file:write_file("/Users/s.elin/Projects/rbkmoney/hellgate/formatted_test.out", io_lib:format("~p:~n~s~n", [RawMeta,FormattedMeta]), [append]),
+    file:write_file("/Users/s.elin/Projects/rbkmoney/hellgate/formatted_test.out", io_lib:format("~s~n", [FormattedMeta]), [append]),
     scoper_woody_event_handler:handle_event(Event, RpcID, RawMeta, Opts).
 
-format_meta(#{args := _Args, type := call} = RawMeta) ->
-%%    _ = dbg:tracer(),
-%%    _ = dbg:p(all, c),
-%%    _ = dbg:tpl({?MODULE, '_', '_'}, x),
-
+format_meta(#{type := call} = RawMeta) ->
     FormattedMeta =
         try
             format_message(RawMeta)
@@ -38,13 +34,33 @@ format_meta(#{args := _Args, type := call} = RawMeta) ->
             _:E:S -> io_lib:format("ERROR: ~p: ~p~n", [E, S])
         end,
     lists:flatten(FormattedMeta);
-format_meta(RawMeta) -> io_lib:format("WARNING: unhandled meta: ~p~n", [RawMeta]).
+format_meta(_RawMeta) -> "". %% RawMeta.
 
-format_message(RawMeta) ->
+format_message(#{result := {ok, Result}, status := ok, type := call} = RawMeta) ->
     {Module, Service} = maps:get(service_schema, RawMeta),
     Service = maps:get(service, RawMeta),
     Function = maps:get(function, RawMeta),
-    FunctionArgs = maps:get(args, RawMeta),
+    case Module:function_info(Service, Function, reply_type) of
+        {struct, struct, []} ->
+            [];
+        {struct, struct, {ReplyModule, ReplyType}} ->
+            io_lib:format("~s", [format_struct(ReplyModule, ReplyType, Result)]);
+        {list, {struct, struct, {ReplyModule, ReplyType}}} ->
+            FormattedList = lists:map(fun(Entry) -> format_struct(ReplyModule, ReplyType, Entry) end, Result),
+            io_lib:format("[~s]", [string:join(FormattedList, ", ")]);
+        _ ->
+            io_lib:format("~p", [Result])
+    end;
+%%format_message(#{result := Result, status := error, type := call} = RawMeta) ->
+%%    {Module, Service} = maps:get(service_schema, RawMeta),
+%%    Service = maps:get(service, RawMeta),
+%%    Function = maps:get(function, RawMeta),
+%%    {struct, struct, ExceptionsMeta} = Module:function_info(Service, Function, exceptions),
+%%    io_lib:format("~s", [format_exception(ExceptionsMeta,Result)]);
+format_message(#{args := FunctionArgs, type := call} = RawMeta) ->
+    {Module, Service} = maps:get(service_schema, RawMeta),
+    Service = maps:get(service, RawMeta),
+    Function = maps:get(function, RawMeta),
     {struct, struct, FunctionArgsMeta} = Module:function_info(Service, Function, params_type),
     io_lib:format("~s:~s(~s)", [Service, Function, format_args(FunctionArgsMeta,FunctionArgs)]).
 
@@ -83,6 +99,17 @@ format_(FunctionArgsMeta, FunctionArgs) ->
                         end, [], ValueList),
                 FormattedValue = string:join(FormattedValueList, ", "),
                 [io_lib:format("~s = [~s]", [Name, FormattedValue]) | Acc];
+            ({{_Fid, _Required, {map, string, {struct, struct,{Module,Struct}}}, Name, _Default}, ValueMap}, Acc) ->
+                MapData = maps:to_list(ValueMap),
+                Result =
+                    lists:foldr(
+                        fun({K, V}, Acc1) ->
+                            [io_lib:format("~s => ~s", [K, format_struct(Module, Struct, V)]) | Acc1]
+                        end,
+                        [], MapData
+                    ),
+                FormattedResult = lists:flatten("#{",[string:join(Result, ", "), "}"]),
+                [io_lib:format("~s = ~s", [Name, FormattedResult]) | Acc];
             ({{_Fid, _Required, _Type, Name, _Default}, Value}, Acc) ->
                 %% All other types such as i32, i64, bool, etc. format here
                 [io_lib:format("~s = ~p", [Name, Value]) | Acc]
@@ -116,6 +143,15 @@ format_union(Module, Struct, {Type, UnionValue}) ->
                     FormattedArgs = format_(UnionMeta, ValueList),
                     lists:flatten([atom_to_list(S), "{", string:join(lists:reverse(FormattedArgs), ", "), "}"])
             end;
+        {value, {_, _, {list, {struct, union, {M, S}}}, Name, _}} ->
+            FormattedValueList =
+                lists:foldr(
+                    fun(Value, FormattedAcc) ->
+                        FormattedUnion = format_union(M, S, Value),
+                        [io_lib:format("~s{~s = '~s'}", [Struct, Name, FormattedUnion])| FormattedAcc]
+                    end, [], UnionValue),
+            FormattedValue = string:join(FormattedValueList, ", "),
+            io_lib:format("~s = [~s]", [Name, FormattedValue]);
         {value, {_, _, {struct, union, {M, S}}, _, _}} ->
             format_union(M, S, UnionValue);
         {value, {_, _, string, Name, _}} when is_binary(UnionValue) ->
@@ -478,7 +514,231 @@ args_test_() -> [
                 type => call}
         )
     )
+].
 
+-spec result_test_() -> _.
+result_test_() -> [
+    ?_assertEqual(
+        lists:flatten([
+            "CallResult{response = <<131,100,0,2,111,107>>, change = MachineStateChange{aux_state = Content{",
+            "data = #{aux_state => \"<<...>>\", ct => \"application/x-erlang-binary\"}}, events = [Content{",
+            "data = [#{ct => \"application/x-erlang-binary\", vsn => 6}, <<...>>]}]}, action = ComplexAction{}}"
+            ]),
+        format_meta(
+            #{
+                deadline => {{{2019,8,13},{11,19,32}},986},
+                execution_start_time => 1565695142994,function => 'ProcessCall',
+                metadata =>
+                #{<<"user-identity.id">> => <<"1CSWG2vduGe">>,
+                    <<"user-identity.realm">> => <<"external">>},
+                result =>
+                {ok,{mg_stateproc_CallResult,
+                    {bin,<<131,100,0,2,111,107>>},
+                    {mg_stateproc_MachineStateChange,
+                        {mg_stateproc_Content,undefined,
+                            {obj,
+                                #{{str,<<"aux_state">>} =>
+                                {bin,
+                                    <<131,116,0,0,0,2,100,0,20,112,97,114,116,
+                                        121,95,114,101,118,105,115,105,111,110,
+                                        95,105,110,100,101,120,116,0,0,0,7,97,0,
+                                        104,2,97,1,97,1,97,1,104,2,97,2,97,2,97,
+                                        2,104,2,97,3,97,3,97,3,104,2,97,4,97,4,
+                                        97,4,104,2,97,5,97,5,97,5,104,2,97,6,97,
+                                        6,97,6,104,2,97,7,97,7,100,0,14,115,110,
+                                        97,112,115,104,111,116,95,105,110,100,
+                                        101,120,106>>},
+                                    {str,<<"ct">>} =>
+                                    {str,<<"application/x-erlang-binary">>}}}},
+                        [{mg_stateproc_Content,undefined,
+                            {arr,
+                                [{obj,
+                                    #{{str,<<"ct">>} =>
+                                    {str,<<"application/x-erlang-binary">>},
+                                        {str,<<"vsn">>} => {i,6}}},
+                                    {bin,
+                                        <<131,104,2,100,0,13,112,97,114,116,121,
+                                            95,99,104,97,110,103,101,115,108,0,0,0,
+                                            2,104,2,100,0,13,115,104,111,112,95,98,
+                                            108,111,99,107,105,110,103,104,3,100,0,
+                                            20,112,97,121,112,114,111,99,95,83,104,
+                                            111,112,66,108,111,99,107,105,110,103,
+                                            109,0,0,0,11,49,67,83,87,71,56,106,48,
+                                            52,119,77,104,2,100,0,7,98,108,111,99,
+                                            107,101,100,104,3,100,0,14,100,111,109,
+                                            97,105,110,95,66,108,111,99,107,101,100,
+                                            109,0,0,0,0,109,0,0,0,27,50,48,49,57,45,
+                                            48,56,45,49,51,84,49,49,58,49,57,58,48,
+                                            51,46,48,49,53,50,50,50,90,104,2,100,0,
+                                            16,114,101,118,105,115,105,111,110,95,
+                                            99,104,97,110,103,101,100,104,3,100,0,
+                                            28,112,97,121,112,114,111,99,95,80,97,
+                                            114,116,121,82,101,118,105,115,105,111,
+                                            110,67,104,97,110,103,101,100,109,0,0,0,
+                                            27,50,48,49,57,45,48,56,45,49,51,84,49,
+                                            49,58,49,57,58,48,51,46,48,49,53,50,50,
+                                            50,90,97,6,106>>}]}}],
+                        undefined,undefined},
+                    {mg_stateproc_ComplexAction,undefined,undefined,undefined,
+                        undefined}}},
+                role => server,service => 'Processor',
+                service_schema => {mg_proto_state_processing_thrift,'Processor'},
+                status => ok,type => call}
+        )
+    ),
+    ?_assertEqual(
+        lists:flatten([
+            "Party{id = '1CSWG2vduGe', contact_info = PartyContactInfo{email = 'hg_ct_helper'}, ",
+            "created_at = '2019-08-13T11:19:01.249440Z', blocking = Unblocked{reason = '', ",
+            "since = '2019-08-13T11:19:02.655869Z'}, suspension = Active{since = '2019-08-13T11:19:02.891892Z'}, ",
+            "contractors = #{}, contracts = #{1CSWG8j04wK => Contract{id = '1CSWG8j04wK', ",
+            "payment_institution = PaymentInstitutionRef{id = 1}, created_at = '2019-08-13T11:19:01.387269Z', ",
+            "status = ContractActive, terms = TermSetHierarchyRef{id = 1}, adjustments = [], ",
+            "payout_tools = [PayoutTool{id = '1CSWG8j04wL', created_at = '2019-08-13T11:19:01.387269Z', ",
+            "currency = CurrencyRef{symbolic_code = 'RUB'}, payout_tool_info = RussianBankAccount{",
+            "account = '4276300010908312893', bank_name = 'SomeBank', bank_post_account = '123129876', ",
+            "bank_bik = '66642666'}}], contractor = RussianLegalEntity{registered_name = 'Hoofs & Horns OJSC', ",
+            "registered_number = '1234509876', inn = '1213456789012', actual_address = 'Nezahualcoyotl 109 Piso 8, ",
+            "Centro, 06082, MEXICO', post_address = 'NaN', representative_position = 'Director', representative_full_name = ",
+            "'Someone', representative_document = '100$ banknote', russian_bank_account = RussianBankAccount{",
+            "account = '4276300010908312893', bank_name = 'SomeBank', bank_post_account = '123129876', ",
+            "bank_bik = '66642666'}}}}, shops = #{1CSWG8j04wM => Shop{id = '1CSWG8j04wM', ",
+            "created_at = '2019-08-13T11:19:01.387269Z', blocking = Blocked{reason = '', ",
+            "since = '2019-08-13T11:19:03.015222Z'}, suspension = Active{since = '2019-08-13T11:19:01.387269Z'}, ",
+            "details = ShopDetails{name = 'Battle Ready Shop'}, location = ShopLocation{url = }, ",
+            "category = CategoryRef{id = 1}, account = ShopAccount{currency = CurrencyRef{symbolic_code = 'RUB'}, ",
+            "settlement = 7, guarantee = 6, payout = 8}, contract_id = '1CSWG8j04wK', payout_tool_id = '1CSWG8j04wL'}}, ",
+            "wallets = #{}, revision = 6}"
+            ]),
+        format_meta(
+            #{args =>
+            [{payproc_UserInfo,<<"1CSWG2vduGe">>,
+                {external_user,{payproc_ExternalUser}}},
+                <<"1CSWG2vduGe">>,
+                {revision,6}],
+                deadline => {{{2019,8,13},{11,19,33}},42},
+                execution_start_time => 1565695143068,function => 'Checkout',
+                metadata =>
+                #{<<"user-identity.id">> => <<"1CSWG2vduGe">>,
+                    <<"user-identity.realm">> => <<"external">>},
+                result =>
+                {ok,
+                    {domain_Party,<<"1CSWG2vduGe">>,
+                        {domain_PartyContactInfo,<<"hg_ct_helper">>},
+                        <<"2019-08-13T11:19:01.249440Z">>,
+                        {unblocked,{domain_Unblocked,<<>>,<<"2019-08-13T11:19:02.655869Z">>}},
+                        {active,{domain_Active,<<"2019-08-13T11:19:02.891892Z">>}},
+                        #{},
+                        #{<<"1CSWG8j04wK">> =>
+                        {domain_Contract,<<"1CSWG8j04wK">>,undefined,
+                            {domain_PaymentInstitutionRef,1},
+                            <<"2019-08-13T11:19:01.387269Z">>,undefined,undefined,
+                            {active,{domain_ContractActive}},
+                            {domain_TermSetHierarchyRef,1},
+                            [],
+                            [{domain_PayoutTool,<<"1CSWG8j04wL">>,
+                                <<"2019-08-13T11:19:01.387269Z">>,
+                                {domain_CurrencyRef,<<"RUB">>},
+                                {russian_bank_account,
+                                    {domain_RussianBankAccount,<<"4276300010908312893">>,
+                                        <<"SomeBank">>,<<"123129876">>,<<"66642666">>}}}],
+                            undefined,undefined,
+                            {legal_entity,
+                                {russian_legal_entity,
+                                    {domain_RussianLegalEntity,<<"Hoofs & Horns OJSC">>,
+                                        <<"1234509876">>,<<"1213456789012">>,
+                                        <<"Nezahualcoyotl 109 Piso 8, Centro, 06082, MEXICO">>,<<"NaN">>,
+                                        <<"Director">>,<<"Someone">>,<<"100$ banknote">>,
+                                        {domain_RussianBankAccount,<<"4276300010908312893">>,
+                                            <<"SomeBank">>,<<"123129876">>,<<"66642666">>}}}}}},
+                        #{<<"1CSWG8j04wM">> =>
+                        {domain_Shop,<<"1CSWG8j04wM">>,<<"2019-08-13T11:19:01.387269Z">>,
+                            {blocked,{domain_Blocked,<<>>,<<"2019-08-13T11:19:03.015222Z">>}},
+                            {active,{domain_Active,<<"2019-08-13T11:19:01.387269Z">>}},
+                            {domain_ShopDetails,<<"Battle Ready Shop">>,undefined},
+                            {url,<<>>},
+                            {domain_CategoryRef,1},
+                            {domain_ShopAccount,{domain_CurrencyRef,<<"RUB">>},7,6,8},
+                            <<"1CSWG8j04wK">>,<<"1CSWG8j04wL">>,undefined}},
+                        #{},6}},
+                role => server,service => 'PartyManagement',
+                service_schema => {dmsl_payment_processing_thrift,'PartyManagement'},
+                status => ok,type => call}
+        )
+    ),
+    ?_assertEqual(
+        lists:flatten([
+            "SignalResult{change = MachineStateChange{aux_state = Content{data = #{}}, ",
+            "events = [Content{data = [[2, #{change => \"created\", ",
+            "contact_info => [35,123,[\"email\",32,61,62,32,\"\\\"create_customer\\\"\"],\"}\"], ",
+            "created_at => \"2019-08-13T11:19:03.714218Z\", customer_id => \"1CSWGJ3N8Ns\", ",
+            "metadata => 'Null', owner_id => \"1CSWG2vduGe\", shop_id => \"1CSWG8j04wM\"}]]}]}, ",
+            "action = ComplexAction{}}"
+        ]),
+        format_meta(
+            #{args =>
+            [{mg_stateproc_SignalArgs,
+                {init,
+                    {mg_stateproc_InitSignal,
+                        {bin,
+                            <<131,109,0,0,0,71,11,0,1,0,0,0,11,49,67,83,87,71,50,118,100,117,71,
+                                101,11,0,2,0,0,0,11,49,67,83,87,71,56,106,48,52,119,77,12,0,3,11,
+                                0,2,0,0,0,15,99,114,101,97,116,101,95,99,117,115,116,111,109,101,
+                                114,0,12,0,4,12,0,1,0,0,0>>}}},
+                {mg_stateproc_Machine,<<"customer">>,<<"1CSWGJ3N8Ns">>,[],
+                    {mg_stateproc_HistoryRange,undefined,undefined,forward},
+                    {mg_stateproc_Content,undefined,{bin,<<>>}},
+                    undefined,
+                    {bin,<<>>}}}],
+                deadline => {{{2019,8,13},{11,19,33}},606},
+                execution_start_time => 1565695143707,function => 'ProcessSignal',
+                metadata =>
+                #{<<"user-identity.id">> => <<"1CSWG2vduGe">>,
+                    <<"user-identity.realm">> => <<"external">>},
+                result =>
+                {ok,
+                    {mg_stateproc_SignalResult,
+                        {mg_stateproc_MachineStateChange,
+                            {mg_stateproc_Content,undefined,{obj,#{}}},
+                            [{mg_stateproc_Content,undefined,
+                                {arr,
+                                    [{arr,
+                                        [{i,2},
+                                            {obj,
+                                                #{{str,<<"change">>} => {str,<<"created">>},
+                                                    {str,<<"contact_info">>} =>
+                                                    {obj,#{{str,<<"email">>} => {str,<<"create_customer">>}}},
+                                                    {str,<<"created_at">>} =>
+                                                    {str,<<"2019-08-13T11:19:03.714218Z">>},
+                                                    {str,<<"customer_id">>} => {str,<<"1CSWGJ3N8Ns">>},
+                                                    {str,<<"metadata">>} => {nl,{mg_msgpack_Nil}},
+                                                    {str,<<"owner_id">>} => {str,<<"1CSWG2vduGe">>},
+                                                    {str,<<"shop_id">>} => {str,<<"1CSWG8j04wM">>}}}]}]}}],
+                            undefined,undefined},
+                        {mg_stateproc_ComplexAction,undefined,undefined,undefined,undefined}}},
+                role => server,service => 'Processor',
+                service_schema => {mg_proto_state_processing_thrift,'Processor'},
+                status => ok,type => call}
+        )
+%%    ),
+%%    ?_assertEqual(
+%%        "InvalidUser{}",
+%%        format_meta(
+%%            #{args =>
+%%            [{payproc_CustomerParams,<<"1CSWGACOup6">>,<<"1CSWGACOup7">>,
+%%                {domain_ContactInfo,undefined,<<"invalid_user">>},
+%%                {nl,{json_Null}}}],
+%%                class => business,deadline => undefined,
+%%                execution_start_time => 1565695141702,function => 'Create',ignore => false,
+%%                metadata =>
+%%                #{<<"user-identity.id">> => <<"1CSWG2vduGe">>,
+%%                    <<"user-identity.realm">> => <<"external">>},
+%%                result => {payproc_InvalidUser},
+%%                role => server,service => 'CustomerManagement',
+%%                service_schema => {dmsl_payment_processing_thrift,'CustomerManagement'},
+%%                status => error,type => call}
+%%        )
+    )
 ].
 
 -endif.
