1c93cd481d0c09581eea6c919a4835c8c4d4d678
Roman Pushkov <pushkovroman@me.com>
2020-05-22 08:26:53 +0300

    uncomment new changes

--- apps/hellgate/src/hg_invoice_payment.erl
+++ apps/hellgate/src/hg_invoice_payment.erl
@@ -2252,12 +2252,12 @@ process_failure({payment, Step} = Activity, Events, Action, Failure, St, _Refund
             _ = maybe_notify_fault_detector(Activity, TargetType, OperationStatus, St),
             process_fatal_payment_failure(Target, Events, Action, Failure, St)
     end;
-process_failure({refund_new, ID}, Events, Action, Failure, St, RefundSt) ->
-    _Clocks = rollback_refund_cashflow(RefundSt, St),
-    {done, {Events ++ [?refund_ev(ID, ?refund_status_changed(?refund_failed(Failure)))], Action}};
-% process_failure({refund_new, ID}, [], Action, Failure, _St, _RefundSt) ->
-%     {next, {[?refund_ev(ID, ?refund_rollback_started(Failure))], hg_machine_action:set_timeout(0, Action)}};
-process_failure({refund_session, ID}, Events, Action, Failure, St, RefundSt) ->
+% process_failure({refund_new, ID}, Events, Action, Failure, St, RefundSt) ->
+%     _Clocks = rollback_refund_cashflow(RefundSt, St),
+%     {done, {Events ++ [?refund_ev(ID, ?refund_status_changed(?refund_failed(Failure)))], Action}};
+process_failure({refund_new, ID}, [], Action, Failure, _St, _RefundSt) ->
+    {next, {[?refund_ev(ID, ?refund_rollback_started(Failure))], hg_machine_action:set_timeout(0, Action)}};
+process_failure({refund_session, ID}, Events, Action, Failure, St, _RefundSt) ->
     Target = ?refunded(),
     case check_retry_possibility(Target, Failure, St) of
         {retry, Timeout} ->
@@ -2265,17 +2265,17 @@ process_failure({refund_session, ID}, Events, Action, Failure, St, RefundSt) ->
             {SessionEvents, SessionAction} = retry_session(Action, Target, Timeout),
             Events1 = [?refund_ev(ID, E) || E <- SessionEvents],
             {next, {Events ++ Events1, SessionAction}};
-        fatal ->
-            _Clocks = rollback_refund_cashflow(RefundSt, St),
-            Events1 = [
-                ?refund_ev(ID, ?refund_status_changed(?refund_failed(Failure)))
-            ],
-            {done, {Events ++ Events1, hg_machine_action:set_timeout(0, Action)}}
         % fatal ->
+        %     _Clocks = rollback_refund_cashflow(RefundSt, St),
         %     Events1 = [
-        %         ?refund_ev(ID, ?refund_rollback_started(Failure))
+        %         ?refund_ev(ID, ?refund_status_changed(?refund_failed(Failure)))
         %     ],
-        %     {next, {Events ++ Events1, hg_machine_action:set_timeout(0, Action)}}
+        %     {done, {Events ++ Events1, hg_machine_action:set_timeout(0, Action)}}
+        fatal ->
+            Events1 = [
+                ?refund_ev(ID, ?refund_rollback_started(Failure))
+            ],
+            {next, {Events ++ Events1, hg_machine_action:set_timeout(0, Action)}}
     end.
 
 choose_fd_operation_status_for_failure({failure, Failure}) ->
@@ -2338,15 +2338,15 @@ process_fatal_payment_failure(?cancelled(), _Events, _Action, Failure, _St) ->
     error({invalid_cancel_failure, Failure});
 process_fatal_payment_failure(?captured(), _Events, _Action, Failure, _St) ->
     error({invalid_capture_failure, Failure});
-% process_fatal_payment_failure(?processed(), [], Action, Failure, _St) ->
-%     {next, {[?payment_rollback_started(Failure)], hg_machine_action:set_timeout(0, Action)}};
-% process_fatal_payment_failure(?processed(), Events, Action, Failure, _St) ->
-%     RollbackStarted = [?payment_rollback_started(Failure)],
-%     {next, {Events ++ RollbackStarted, hg_machine_action:set_timeout(0, Action)}};
-process_fatal_payment_failure(_Target, Events, Action, Failure, St) ->
-    _Clocks = rollback_payment_cashflow(St),
-    PaymentFailed = [?payment_status_changed(?failed(Failure))],
-    {done, {Events ++ PaymentFailed, hg_machine_action:set_timeout(0, Action)}}.
+process_fatal_payment_failure(?processed(), [], Action, Failure, _St) ->
+    {next, {[?payment_rollback_started(Failure)], hg_machine_action:set_timeout(0, Action)}};
+process_fatal_payment_failure(?processed(), Events, Action, Failure, _St) ->
+    RollbackStarted = [?payment_rollback_started(Failure)],
+    {next, {Events ++ RollbackStarted, hg_machine_action:set_timeout(0, Action)}}.
+% process_fatal_payment_failure(_Target, Events, Action, Failure, St) ->
+%     _Clocks = rollback_payment_cashflow(St),
+%     PaymentFailed = [?payment_status_changed(?failed(Failure))],
+%     {done, {Events ++ PaymentFailed, hg_machine_action:set_timeout(0, Action)}}.
 
 retry_session(Action, Target, Timeout) ->
     NewEvents = start_session(Target),
