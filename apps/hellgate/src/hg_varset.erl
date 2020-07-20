eb63ef685502f244fc6caa3941e2cae4e3cb71e8
r.shaidullin <ndiezel0@gmail.com>
2020-07-20 21:15:21 +0300

    HG-452: Fix tests

--- apps/hellgate/src/hg_varset.erl
+++ apps/hellgate/src/hg_varset.erl
@@ -24,13 +24,14 @@
 -spec prepare_varset(varset()) -> dmsl_payment_processing_thrift:'Varset'().
 
 prepare_varset(Varset) ->
-    #payproc_Varset{
-        category = genlib_map:get(category, Varset),
-        currency = genlib_map:get(currency, Varset),
-        amount = genlib_map:get(cost, Varset),
-        payment_method = hg_maybe:apply(
-            fun(X) -> hg_payment_tool:get_method(X) end, genlib_map:get(payment_tool, Varset)),
-        payout_method = genlib_map:get(payout_method, Varset),
-        wallet_id = genlib_map:get(wallet_id, Varset),
-        p2p_tool = genlib_map:get(p2p_tool, Varset)
+     #payproc_Varset{
+         category = genlib_map:get(category, Varset),
+         currency = genlib_map:get(currency, Varset),
+         amount = genlib_map:get(cost, Varset),
+         payment_method = hg_maybe:apply(
+             fun(X) -> hg_payment_tool:get_method(X) end, genlib_map:get(payment_tool, Varset)),
+         payout_method = genlib_map:get(payout_method, Varset),
+         wallet_id = genlib_map:get(wallet_id, Varset),
+         p2p_tool = genlib_map:get(p2p_tool, Varset),
+         payment_tool = genlib_map:get(payment_tool, Varset)
     }.
