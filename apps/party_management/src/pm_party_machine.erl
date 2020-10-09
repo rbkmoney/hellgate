3f87d39406bc3ed17f50dc4634bbf52aeab289e9
r.shaidullin <ndiezel0@gmail.com>
2020-10-09 12:41:06 +0300

    HG-452: Add caching for Checkout Party

--- apps/party_management/src/pm_party_machine.erl
+++ apps/party_management/src/pm_party_machine.erl
@@ -829,21 +829,28 @@ checkout_history_by_timestamp([], Timestamp, St) ->
     {ok, St#st{timestamp = Timestamp}}.
 
 checkout_party_by_revision(PartyID, Revision) ->
-    AuxSt = get_aux_state(PartyID),
-    FromEventID =
-        case get_party_revision_range(Revision, get_party_revision_index(AuxSt)) of
-            {_, undefined} ->
-                undefined;
-            {_, EventID} ->
-                EventID + 1
-        end,
-    Limit = get_limit(FromEventID, get_snapshot_index(AuxSt)),
-    ReversedHistory = get_history(PartyID, FromEventID, Limit, backward),
-    case parse_history(ReversedHistory) of
-        {undefined, Events} ->
-            checkout_history_by_revision(Events, Revision, #st{});
-        {St, Events} ->
-            checkout_history_by_revision(Events, Revision, St)
+    case get_party_from_cache({PartyID, Revision}) of
+        {ok, Party} ->
+            {ok, Party};
+        not_found ->
+            AuxSt = get_aux_state(PartyID),
+            FromEventID =
+                case get_party_revision_range(Revision, get_party_revision_index(AuxSt)) of
+                    {_, undefined} ->
+                        undefined;
+                    {_, EventID} ->
+                        EventID + 1
+                end,
+            Limit = get_limit(FromEventID, get_snapshot_index(AuxSt)),
+            ReversedHistory = get_history(PartyID, FromEventID, Limit, backward),
+            {ok, Party} = case parse_history(ReversedHistory) of
+                {undefined, Events} ->
+                    checkout_history_by_revision(Events, Revision, #st{});
+                {St, Events} ->
+                    checkout_history_by_revision(Events, Revision, St)
+            end,
+            ok = update_party_cache({PartyID, Revision}, Party),
+            {ok, Party}
     end.
 
 checkout_history_by_revision([Ev | Rest], Revision, St) ->
@@ -1795,3 +1802,17 @@ transmute_payout_schedule_ref(3, 4, ?legacy_payout_schedule_ref(ID)) ->
     #domain_BusinessScheduleRef{id = ID};
 transmute_payout_schedule_ref(3, 4, undefined) ->
     undefined.
+
+-spec get_party_from_cache({party_id(), party_revision_param()}) ->
+    not_found | {ok, party()}.
+get_party_from_cache(Key) ->
+    case cache:get(party, Key) of
+        undefined ->
+            not_found;
+        {_, Value} ->
+            {ok, Value}
+    end.
+
+-spec update_party_cache({party_id(), party_revision_param()}, party()) -> ok.
+update_party_cache(Key, Value) ->
+    cache:put(party, Key, Value).
