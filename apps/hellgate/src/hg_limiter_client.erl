2b564ba00eca9799462bf709176da19ab796409c
0x42 <bpozdnyakov@gmail.com>
2021-03-01 09:47:22 +0300

    del logging woody errors

--- apps/hellgate/src/hg_limiter_client.erl
+++ apps/hellgate/src/hg_limiter_client.erl
@@ -24,94 +24,69 @@
 get(LimitID, Timestamp) ->
     Args = {LimitID, Timestamp},
     Opts = hg_woody_wrapper:get_service_options(limiter),
-    try
-        case hg_woody_wrapper:call(limiter, 'Get', Args, Opts) of
-            {ok, Limit} ->
-                Limit;
-            {exception, #proto_limiter_LimitNotFound{}} ->
-                error({not_found, LimitID});
-            {exception, #'InvalidRequest'{errors = Errors}} ->
-                error({invalid_request, Errors})
-        end
-    catch
-        error:{woody_error, {_Source, _Class, _Details}} = Reason ->
-            error(Reason)
+    case hg_woody_wrapper:call(limiter, 'Get', Args, Opts) of
+        {ok, Limit} ->
+            Limit;
+        {exception, #proto_limiter_LimitNotFound{}} ->
+            error({not_found, LimitID});
+        {exception, #'InvalidRequest'{errors = Errors}} ->
+            error({invalid_request, Errors})
     end.
 
 -spec hold(limit_change()) -> ok.
 hold(LimitChange) ->
     LimitID = LimitChange#proto_limiter_LimitChange.id,
     Opts = hg_woody_wrapper:get_service_options(limiter),
-    try
-        case hg_woody_wrapper:call(limiter, 'Hold', {LimitChange}, Opts) of
-            {ok, ok} ->
-                ok;
-            {exception, #proto_limiter_LimitNotFound{}} ->
-                error({not_found, LimitID});
-            {exception, #'InvalidRequest'{errors = Errors}} ->
-                error({invalid_request, Errors})
-        end
-    catch
-        error:{woody_error, {_Source, _Class, _Details}} = Reason ->
-            error(Reason)
+    case hg_woody_wrapper:call(limiter, 'Hold', {LimitChange}, Opts) of
+        {ok, ok} ->
+            ok;
+        {exception, #proto_limiter_LimitNotFound{}} ->
+            error({not_found, LimitID});
+        {exception, #'InvalidRequest'{errors = Errors}} ->
+            error({invalid_request, Errors})
     end.
 
 -spec commit(limit_change()) -> ok.
 commit(LimitChange) ->
     Opts = hg_woody_wrapper:get_service_options(limiter),
-    try
-        case hg_woody_wrapper:call(limiter, 'Commit', {LimitChange}, Opts) of
-            {ok, ok} ->
-                ok;
-            {exception, #proto_limiter_LimitNotFound{}} ->
-                error({not_found, LimitChange#proto_limiter_LimitChange.id});
-            {exception, #proto_limiter_LimitChangeNotFound{}} ->
-                error({not_found, {limit_change, LimitChange#proto_limiter_LimitChange.change_id}});
-            {exception, #'InvalidRequest'{errors = Errors}} ->
-                error({invalid_request, Errors})
-        end
-    catch
-        error:{woody_error, {_Source, _Class, _Details}} = Reason ->
-            error(Reason)
+    case hg_woody_wrapper:call(limiter, 'Commit', {LimitChange}, Opts) of
+        {ok, ok} ->
+            ok;
+        {exception, #proto_limiter_LimitNotFound{}} ->
+            error({not_found, LimitChange#proto_limiter_LimitChange.id});
+        {exception, #proto_limiter_LimitChangeNotFound{}} ->
+            error({not_found, {limit_change, LimitChange#proto_limiter_LimitChange.change_id}});
+        {exception, #'InvalidRequest'{errors = Errors}} ->
+            error({invalid_request, Errors})
     end.
 
 -spec partial_commit(limit_change()) -> ok.
 partial_commit(LimitChange) ->
     Opts = hg_woody_wrapper:get_service_options(limiter),
-    try
-        case hg_woody_wrapper:call(limiter, 'PartialCommit', {LimitChange}, Opts) of
-            {ok, ok} ->
-                ok;
-            {exception, #proto_limiter_LimitNotFound{}} ->
-                error({not_found, LimitChange#proto_limiter_LimitChange.id});
-            {exception, #proto_limiter_LimitChangeNotFound{}} ->
-                error({not_found, {limit_change, LimitChange#proto_limiter_LimitChange.change_id}});
-            {exception, #proto_limiter_ForbiddenOperationAmount{amount = Amount, allowed_range = CashRange}} ->
-                error({forbidden_operation_amount, {Amount, CashRange}});
-            {exception, #'InvalidRequest'{errors = Errors}} ->
-                error({invalid_request, Errors})
-        end
-    catch
-        error:{woody_error, {_Source, _Class, _Details}} = Reason ->
-            error(Reason)
+    case hg_woody_wrapper:call(limiter, 'PartialCommit', {LimitChange}, Opts) of
+        {ok, ok} ->
+            ok;
+        {exception, #proto_limiter_LimitNotFound{}} ->
+            error({not_found, LimitChange#proto_limiter_LimitChange.id});
+        {exception, #proto_limiter_LimitChangeNotFound{}} ->
+            error({not_found, {limit_change, LimitChange#proto_limiter_LimitChange.change_id}});
+        {exception, #proto_limiter_ForbiddenOperationAmount{amount = Amount, allowed_range = CashRange}} ->
+            error({forbidden_operation_amount, {Amount, CashRange}});
+        {exception, #'InvalidRequest'{errors = Errors}} ->
+            error({invalid_request, Errors})
     end.
 
 -spec rollback(limit_change()) -> ok.
 rollback(LimitChange) ->
     LimitID = LimitChange#proto_limiter_LimitChange.id,
     Opts = hg_woody_wrapper:get_service_options(limiter),
-    try
-        case hg_woody_wrapper:call(limiter, 'Rollback', {LimitChange}, Opts) of
-            {ok, ok} ->
-                ok;
-            {exception, #proto_limiter_LimitNotFound{}} ->
-                error({not_found, LimitID});
-            {exception, #proto_limiter_LimitChangeNotFound{}} ->
-                error({not_found, {limit_change, LimitChange#proto_limiter_LimitChange.change_id}});
-            {exception, #'InvalidRequest'{errors = Errors}} ->
-                error({invalid_request, Errors})
-        end
-    catch
-        error:{woody_error, {_Source, _Class, _Details}} = Reason ->
-            error(Reason)
+    case hg_woody_wrapper:call(limiter, 'Rollback', {LimitChange}, Opts) of
+        {ok, ok} ->
+            ok;
+        {exception, #proto_limiter_LimitNotFound{}} ->
+            error({not_found, LimitID});
+        {exception, #proto_limiter_LimitChangeNotFound{}} ->
+            error({not_found, {limit_change, LimitChange#proto_limiter_LimitChange.change_id}});
+        {exception, #'InvalidRequest'{errors = Errors}} ->
+            error({invalid_request, Errors})
     end.
