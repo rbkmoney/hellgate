7c48aef1388b17241ace6686319b1ef462f40d07
Sergey Yelin <elinsn@gmail.com>
2021-04-06 13:16:12 +0300

    Remove unused test_condition/3

--- apps/hellgate/src/hg_payment_tool.erl
+++ apps/hellgate/src/hg_payment_tool.erl
@@ -6,7 +6,6 @@
 
 %%
 
--export([test_condition/3]).
 -export([has_any_payment_method/2]).
 -export([get_possible_methods/1]).
 
@@ -16,7 +15,6 @@
 
 -type t() :: dmsl_domain_thrift:'PaymentTool'().
 -type method() :: dmsl_domain_thrift:'PaymentMethodRef'().
--type condition() :: dmsl_domain_thrift:'PaymentToolCondition'().
 
 -spec has_any_payment_method(t(), ordsets:ordset(method())) -> boolean().
 has_any_payment_method(PaymentTool, SupportedMethods) ->
@@ -89,137 +87,6 @@ create_bank_card_payment_method_ref(#domain_BankCard{
                 tokenization_method = TokenizationMethod
             }}
     }.
-
-%%
-
--spec test_condition(condition(), t(), hg_domain:revision()) -> boolean() | undefined.
-test_condition({bank_card, C}, {bank_card, V = #domain_BankCard{}}, Rev) ->
-    test_bank_card_condition(C, V, Rev);
-test_condition({payment_terminal, C}, {payment_terminal, V = #domain_PaymentTerminal{}}, Rev) ->
-    test_payment_terminal_condition(C, V, Rev);
-test_condition({digital_wallet, C}, {digital_wallet, V = #domain_DigitalWallet{}}, Rev) ->
-    test_digital_wallet_condition(C, V, Rev);
-test_condition({crypto_currency, C}, {crypto_currency_deprecated, V}, Rev) ->
-    test_crypto_currency_condition(C, V, Rev);
-test_condition({mobile_commerce, C}, {mobile_commerce, V}, Rev) ->
-    test_mobile_commerce_condition(C, V, Rev);
-test_condition(_PaymentTool, _Condition, _Rev) ->
-    false.
-
-test_bank_card_condition(#domain_BankCardCondition{definition = Def}, V, Rev) when Def /= undefined ->
-    test_bank_card_condition_def(Def, V, Rev);
-test_bank_card_condition(#domain_BankCardCondition{}, _, _Rev) ->
-    true.
-
-% legacy
-test_bank_card_condition_def(
-    {payment_system_is, Ps},
-    #domain_BankCard{payment_system = Ps, token_provider_deprecated = undefined},
-    _Rev
-) ->
-    true;
-test_bank_card_condition_def({payment_system_is, _Ps}, #domain_BankCard{}, _Rev) ->
-    false;
-test_bank_card_condition_def({payment_system, PaymentSystem}, V, Rev) ->
-    test_payment_system_condition(PaymentSystem, V, Rev);
-test_bank_card_condition_def({issuer_country_is, IssuerCountry}, V, Rev) ->
-    test_issuer_country_condition(IssuerCountry, V, Rev);
-test_bank_card_condition_def({issuer_bank_is, BankRef}, V, Rev) ->
-    test_issuer_bank_condition(BankRef, V, Rev);
-test_bank_card_condition_def(
-    {empty_cvv_is, Val},
-    #domain_BankCard{is_cvv_empty = Val},
-    _Rev
-) ->
-    true;
-%% Для обратной совместимости с картами, у которых нет is_cvv_empty
-test_bank_card_condition_def(
-    {empty_cvv_is, false},
-    #domain_BankCard{is_cvv_empty = undefined},
-    _Rev
-) ->
-    true;
-test_bank_card_condition_def({empty_cvv_is, _Val}, #domain_BankCard{}, _Rev) ->
-    false.
-
-test_payment_system_condition(
-    #domain_PaymentSystemCondition{
-        payment_system_is_deprecated = Ps,
-        token_provider_is_deprecated = Tp,
-        tokenization_method_is = TmCond
-    },
-    #domain_BankCard{payment_system_deprecated = Ps, token_provider_deprecated = Tp, tokenization_method = Tm},
-    _Rev
-) ->
-    test_tokenization_method_condition(TmCond, Tm);
-test_payment_system_condition(#domain_PaymentSystemCondition{}, #domain_BankCard{}, _Rev) ->
-    false.
-
-test_tokenization_method_condition(undefined, _) ->
-    true;
-test_tokenization_method_condition(_NotUndefined, undefined) ->
-    undefined;
-test_tokenization_method_condition(DesiredMethod, ActualMethod) ->
-    DesiredMethod == ActualMethod.
-
-test_issuer_country_condition(_Country, #domain_BankCard{issuer_country = undefined}, _Rev) ->
-    undefined;
-test_issuer_country_condition(Country, #domain_BankCard{issuer_country = TargetCountry}, _Rev) ->
-    Country == TargetCountry.
-
-test_issuer_bank_condition(BankRef, #domain_BankCard{bank_name = BankName, bin = BIN}, Rev) ->
-    #domain_Bank{binbase_id_patterns = Patterns, bins = BINs} = hg_domain:get(Rev, {bank, BankRef}),
-    case {Patterns, BankName} of
-        {P, B} when is_list(P) and is_binary(B) ->
-            test_bank_card_patterns(Patterns, BankName);
-        % TODO т.к. BinBase не обладает полным объемом данных, при их отсутствии мы возвращаемся к проверкам по бинам.
-        %      B будущем стоит избавиться от этого.
-        {_, _} ->
-            test_bank_card_bins(BIN, BINs)
-    end.
-
-test_bank_card_bins(BIN, BINs) ->
-    ordsets:is_element(BIN, BINs).
-
-test_bank_card_patterns(Patterns, BankName) ->
-    Matches = ordsets:filter(fun(E) -> genlib_wildcard:match(BankName, E) end, Patterns),
-    ordsets:size(Matches) > 0.
-
-test_payment_terminal_condition(#domain_PaymentTerminalCondition{definition = Def}, V, Rev) ->
-    Def =:= undefined orelse test_payment_terminal_condition_def(Def, V, Rev).
-
-test_payment_terminal_condition_def(
-    {provider_is_deprecated, V1},
-    #domain_PaymentTerminal{terminal_type_deprecated = V2},
-    _Rev
-) ->
-    V1 =:= V2.
-
-test_digital_wallet_condition(#domain_DigitalWalletCondition{definition = Def}, V, Rev) ->
-    Def =:= undefined orelse test_digital_wallet_condition_def(Def, V, Rev).
-
-test_digital_wallet_condition_def(
-    {provider_is_deprecated, V1},
-    #domain_DigitalWallet{provider_deprecated = V2},
-    _Rev
-) ->
-    V1 =:= V2.
-
-test_crypto_currency_condition(#domain_CryptoCurrencyCondition{definition = Def}, V, Rev) ->
-    Def =:= undefined orelse test_crypto_currency_condition_def(Def, V, Rev).
-
-test_crypto_currency_condition_def({crypto_currency_is_deprecated, C1}, C2, _Rev) ->
-    C1 =:= C2.
-
-test_mobile_commerce_condition(#domain_MobileCommerceCondition{definition = Def}, V, Rev) ->
-    Def =:= undefined orelse test_mobile_commerce_condition_def(Def, V, Rev).
-
-test_mobile_commerce_condition_def(
-    {operator_is_deprecated, C1},
-    #domain_MobileCommerce{operator_deprecated = C2},
-    _Rev
-) ->
-    C1 =:= C2.
 
 %% Unmarshalling
 
