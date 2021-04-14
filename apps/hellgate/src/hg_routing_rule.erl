2a81e1ad8e506aa67a0e4cf53a51dd68a5639eeb
Yuri Bukhalenkov <y.buhalenkov@rbk.money>
2021-04-14 14:13:06 +0300

    merge from master (part II)

--- apps/hellgate/src/hg_routing_rule.erl
+++ apps/hellgate/src/hg_routing_rule.erl
@@ -32,42 +32,23 @@ gather_routes(Predestination, PaymentInstitution, VS, Revision) ->
         rejected_routes => []
     },
     PaymentRouting = PaymentInstitution#domain_PaymentInstitution.payment_routing_rules,
-    Candidates = get_candidates(master, PaymentRouting#domain_RoutingRules.policies, VS, Revision),
+    Candidates = get_candidates(PaymentRouting#domain_RoutingRules.policies, VS, Revision),
     RatedRoutes = collect_routes(Predestination, Candidates, VS, Revision),
     RuleSetDeny = get_rule_set(PaymentRouting#domain_RoutingRules.prohibitions, Revision),
-    Prohibitions = get_table_prohibitions(master, RuleSetDeny, VS, Revision),
+    Prohibitions = get_table_prohibitions(RuleSetDeny, VS, Revision),
     {Accepted, RejectedRoutes} = filter_routes(RatedRoutes, Prohibitions),
     {Accepted, RejectedContext#{rejected_routes => RejectedRoutes}}.
 
-%% TODO: from working branch
-get_table_prohibitions(branch, RuleSetDeny, _, _) ->
-    Candidates = get_decisions_candidates(RuleSetDeny),
+get_table_prohibitions(RuleSetDeny, _, _) ->
     lists:foldr(
         fun(#domain_RoutingCandidate{terminal = K, description = V}, AccIn) ->
             AccIn#{K => V}
         end,
         #{},
-        Candidates
-    );
-%% TODO: from master
-get_table_prohibitions(master, RuleSetDeny, VS, Revision) ->
-    {Candidates, _} = reduce(RuleSetDeny, VS, Revision, []),
-    lists:foldl(
-        fun(#domain_RoutingCandidate{terminal = K, description = V}, AccIn) ->
-            AccIn#{K => V}
-        end,
-        #{},
-        Candidates
+        get_decisions_candidates(RuleSetDeny)
     ).
 
-%% TODO: from master
-get_candidates(master, RoutingRuleRef, VS, Revision) ->
-    RuleSet = get_rule_set(RoutingRuleRef, Revision),
-    {Candidates, RoutingRuleTrace} = reduce(RuleSet, VS, Revision, [RoutingRuleRef]),
-    logger:info("Routing computation trace: ~p. Candidates: ~p", [lists:reverse(RoutingRuleTrace), Candidates]),
-    Candidates;
-%% TODO: from working branch
-get_candidates(branch, RoutingRuleRef, VS, Revision) ->
+get_candidates(RoutingRuleRef, VS, Revision) ->
     get_decisions_candidates(
         compute_rule_set(RoutingRuleRef, VS, Revision)
     ).
@@ -83,69 +64,10 @@ get_decisions_candidates(#domain_RoutingRuleset{decisions = Decisions}) ->
 
 validate_decisions_candidates([]) ->
     ok;
-validate_decisions_candidates([Candidate | Rest]) ->
-    #domain_RoutingCandidate{
-        allowed = Predicate
-    } = Candidate,
-    case Predicate of
-        {constant, true} ->
-            validate_decisions_candidates(Rest);
-        _ ->
-            error({misconfiguration, {'PaymentRoutingCandidate couldn\'t be reduced', Candidate}})
-    end.
-
-reduce(RuleSet, VS, Revision, RoutingRuleTrace) ->
-    #domain_RoutingRuleset{
-        decisions = Decisions
-    } = RuleSet,
-    reduce_decisions(Decisions, VS, Revision, RoutingRuleTrace).
-
-reduce_decisions({_, []}, _, _, RoutingRuleTrace) ->
-    {[], RoutingRuleTrace};
-reduce_decisions({delegates, Delegates}, VS, Rev, Trace) ->
-    reduce_delegates_decision(Delegates, VS, Rev, Trace);
-reduce_decisions({candidates, C}, VS, Rev, Trace) ->
-    Candidates = reduce_candidates_decision(C, VS, Rev),
-    {Candidates, Trace}.
-
-reduce_delegates_decision([], _VS, _Rev, Trace) ->
-    {[], Trace};
-reduce_delegates_decision([D | Delegates], VS, Rev, Trace) ->
-    Predicate = D#domain_RoutingDelegate.allowed,
-    RuleSetRef = D#domain_RoutingDelegate.ruleset,
-    case pm_selector:reduce_predicate(Predicate, VS, Rev) of
-        ?const(false) ->
-            reduce_delegates_decision(Delegates, VS, Rev, Trace);
-        ?const(true) ->
-            reduce(get_rule_set(RuleSetRef, Rev), VS, Rev, [RuleSetRef | Trace]);
-        _ ->
-            logger:warning(
-                "Routing rule misconfiguration, can't reduce decision. Predicate: ~p~n Varset:~n~p",
-                [Predicate, VS]
-            ),
-            {[], Trace}
-    end.
-
-reduce_candidates_decision(Candidates, VS, Rev) ->
-    lists:foldl(
-        fun(C, AccIn) ->
-            Predicate = C#domain_RoutingCandidate.allowed,
-            case pm_selector:reduce_predicate(Predicate, VS, Rev) of
-                ?const(false) ->
-                    AccIn;
-                ?const(true) ->
-                    [C | AccIn];
-                _ ->
-                    logger:warning(
-                        "Routing rule misconfiguration, can't reduce decision. Predicate: ~p~nVarset:~n~p",
-                        [Predicate, VS]
-                    ),
-                    AccIn
-            end
-        end,
-        [],
-        Candidates
-    ).
+validate_decisions_candidates([#domain_RoutingCandidate{allowed = {constant, true}} | Rest]) ->
+    validate_decisions_candidates(Rest);
+validate_decisions_candidates([Candidate | _]) ->
+    error({misconfiguration, {'PaymentRoutingCandidate couldn\'t be reduced', Candidate}}).
 
 collect_routes(Predestination, Candidates, VS, Revision) ->
     lists:foldr(
@@ -191,11 +113,10 @@ filter_routes({Routes, Rejected}, Prohibitions) ->
 
 compute_rule_set(RuleSetRef, VS, Revision) ->
     {Client, Context} = get_party_client(),
-    PreparedVarset = hg_varset:prepare_varset(VS),
-    {ok, RuleSet} = party_client_thrift:compute_payment_routing_ruleset(
+    {ok, RuleSet} = party_client_thrift:compute_routing_ruleset(
         RuleSetRef,
         Revision,
-        PreparedVarset,
+        hg_varset:prepare_varset(VS),
         Client,
         Context
     ),
