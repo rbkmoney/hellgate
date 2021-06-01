rbkmoney/hellgate

commit 016faf528c97fc47ccbc19985fcc3381ce4b79cf

Bump to rbkmoney/damsel@cc2875b carefully (#571)
* Adapt party machine transmutations facilities to support it
* Encode party state snapshots as thrift binary
* Avoid excessive party event decoding
* Add eventsink consistency testcase to partymgmt

apps/party_management/test/pm_party_tests_SUITE.erl
@@ -107,6 +107,8 @@
 -export([compute_pred_w_irreducible_criterion/1]).
 -export([compute_terms_w_criteria/1]).
 
+-export([consistent_eventsink_history/1]).
+
 %% tests descriptions
 
 -type config() :: pm_ct_helper:config().
@@ -132,7 +134,9 @@ all() ->
 
         {group, claim_management},
         {group, compute},
-        {group, terms}
+        {group, terms},
+
+        consistent_eventsink_history
     ].
 
 -spec groups() -> [{group_name(), list(), [test_case_name()]}].
@@ -535,6 +539,8 @@ end_per_testcase(_Name, _C) ->
 
 -spec compute_pred_w_irreducible_criterion(config()) -> _ | no_return().
 -spec compute_terms_w_criteria(config()) -> _ | no_return().
+
+-spec consistent_eventsink_history(config()) -> _ | no_return().
 
 party_creation(C) ->
     Client = cfg(client, C),
@@ -1939,6 +1945,11 @@ compute_terms_w_criteria(C) ->
             )
         end
     ).
+
+consistent_eventsink_history(C) ->
+    Client = pm_client_eventsink:start_link(pm_client_api:new(cfg(root_url, C))),
+    Events = pm_client_eventsink:pull_events(5000, 1000, Client),
+    ok = pm_eventsink_history:assert_total_order(Events).
 
 %%
 

apps/party_management/include/legacy_party_structures.hrl
@@ -67,6 +67,16 @@
     {domain_InternationalLegalEntity, LegalName, TradingName, RegisteredAddress, ActualAddress}
 ).
 
+-define(legacy_international_legal_entity_v2(
+    LegalName,
+    TradingName,
+    RegisteredAddress,
+    ActualAddress,
+    RegisteredNumber
+),
+    {domain_InternationalLegalEntity, LegalName, TradingName, RegisteredAddress, ActualAddress, RegisteredNumber}
+).
+
 -define(legacy_bank_account(Account, BankName, BankPostAccount, BankBik),
     {domain_BankAccount, Account, BankName, BankPostAccount, BankBik}
 ).
@@ -200,6 +210,25 @@
     LegalAgreementID
 ),
     {domain_LegalAgreement, SignedAt, LegalAgreementID}
+).
+
+-define(legacy_st(Party, Timestamp, Claims, Meta, MigrationData, LastEvent),
+    {st,
+        % undefined | party()
+        Party,
+        % undefined | timestamp()
+        Timestamp,
+        % #{claim_id() => claim()}
+        Claims,
+        % meta()
+        Meta,
+        % NOTE
+        % This is a part of persisted state of almost every party machine out there.
+        % Good news is this field was never really used which means it is just `#{}`
+        % all the time.
+        MigrationData,
+        % event_id()
+        LastEvent}
 ).
 
 -endif.

apps/party_management/include/party_events.hrl
@@ -5,6 +5,11 @@
 
 -define(party_ev(PartyChanges), {party_changes, PartyChanges}).
 
+-define(party_event_data(PartyChanges, Snapshot), #payproc_PartyEventData{
+    changes = PartyChanges,
+    state_snapshot = Snapshot
+}).
+
 -define(party_created(PartyID, ContactInfo, Timestamp),
     {party_created, #payproc_PartyCreated{
         id = PartyID,

apps/party_management/src/pm_maybe.erl
@@ -11,11 +11,11 @@
 
 -export_type([maybe/1]).
 
--spec apply(fun(), Arg :: undefined | term()) -> term().
+-spec apply(fun((T) -> U), maybe(T)) -> maybe(U).
 apply(Fun, Arg) ->
     pm_maybe:apply(Fun, Arg, undefined).
 
--spec apply(fun(), Arg :: undefined | term(), Default :: term()) -> term().
+-spec apply(fun((T) -> U), maybe(T), Default) -> U | Default.
 apply(Fun, Arg, _Default) when Arg =/= undefined ->
     Fun(Arg);
 apply(_Fun, undefined, Default) ->

apps/party_management/test/pm_ct_helper.erl
@@ -190,6 +190,13 @@ start_app(party_management = AppName) ->
                         pool => claim_committer,
                         max_connections => 300
                     }
+                },
+                payment_processing_eventsink => #{
+                    url => <<"http://hellgate:8022/v1/processing/eventsink">>,
+                    transport_opts => #{
+                        pool => payment_processing_eventsink,
+                        max_connections => 300
+                    }
                 }
             }}
         ]), #{}};

apps/party_management/test/pm_eventsink_history.erl
@@ -1,0 +1,29 @@
+-module(pm_eventsink_history).
+
+-export([assert_total_order/1]).
+
+%%
+
+-include_lib("damsel/include/dmsl_payment_processing_thrift.hrl").
+
+-type history() :: [dmsl_payment_processing_thrift:'Event'()].
+
+-define(event(ID, Source, Payload), #payproc_Event{
+    id = ID,
+    source = Source,
+    payload = Payload
+}).
+
+-spec assert_total_order(history()) -> ok | no_return().
+assert_total_order([]) ->
+    ok;
+assert_total_order([?event(ID, _, _) | Rest]) ->
+    _ = lists:foldl(
+        fun(?event(ID1, _, _), ID0) ->
+            true = ID1 > ID0,
+            ID1
+        end,
+        ID,
+        Rest
+    ),
+    ok.

apps/pm_client/src/pm_client_eventsink.erl
@@ -1,0 +1,144 @@
+-module(pm_client_eventsink).
+
+-include_lib("damsel/include/dmsl_payment_processing_thrift.hrl").
+
+-export([start/1]).
+-export([start_link/1]).
+-export([stop/1]).
+
+-export([get_last_event_id/1]).
+-export([pull_events/2]).
+-export([pull_events/3]).
+-export([pull_history/1]).
+
+%% GenServer
+
+-behaviour(gen_server).
+
+-export([init/1]).
+-export([handle_call/3]).
+-export([handle_cast/2]).
+-export([handle_info/2]).
+-export([terminate/2]).
+-export([code_change/3]).
+
+%%
+
+-type event_id() :: dmsl_base_thrift:'EventID'().
+
+-spec start(pm_client_api:t()) -> pid().
+start(ApiClient) ->
+    start(start, ApiClient).
+
+-spec start_link(pm_client_api:t()) -> pid().
+start_link(ApiClient) ->
+    start(start_link, ApiClient).
+
+start(Mode, ApiClient) ->
+    {ok, Pid} = gen_server:Mode(?MODULE, ApiClient, []),
+    Pid.
+
+-spec stop(pid()) -> ok.
+stop(Client) ->
+    _ = exit(Client, shutdown),
+    ok.
+
+%%
+
+-define(DEFAULT_POLL_TIMEOUT, 1000).
+
+-spec get_last_event_id(pid()) -> event_id() | none | woody_error:business_error().
+get_last_event_id(Client) ->
+    case gen_server:call(Client, {call, 'GetLastEventID', []}) of
+        {ok, EventID} when is_integer(EventID) ->
+            EventID;
+        {exception, #payproc_NoLastEvent{}} ->
+            none;
+        Error ->
+            Error
+    end.
+
+-spec pull_events(pos_integer(), pid()) -> [tuple()] | woody_error:business_error().
+pull_events(N, Client) when N > 0 ->
+    pull_events(N, ?DEFAULT_POLL_TIMEOUT, Client).
+
+-spec pull_events(pos_integer(), timeout(), pid()) -> [tuple()] | woody_error:business_error().
+pull_events(N, Timeout, Client) when N > 0 ->
+    gen_server:call(Client, {pull_events, N, Timeout}, infinity).
+
+-spec pull_history(pid()) -> [tuple()] | woody_error:business_error().
+pull_history(Client) ->
+    gen_server:call(Client, {pull_history, 1000}, infinity).
+
+%%
+
+-type event() :: dmsl_payment_processing_thrift:'Event'().
+
+-record(state, {
+    poller :: pm_client_event_poller:st(event()),
+    client :: pm_client_api:t()
+}).
+
+-type state() :: #state{}.
+-type callref() :: {pid(), Tag :: reference()}.
+
+-define(SERVICE, payment_processing_eventsink).
+
+-spec init(pm_client_api:t()) -> {ok, state()}.
+init(ApiClient) ->
+    {ok, #state{
+        client = ApiClient,
+        poller = pm_client_event_poller:new(
+            {?SERVICE, 'GetEvents', []},
+            fun(Event) -> Event#payproc_Event.id end
+        )
+    }}.
+
+-spec handle_call(term(), callref(), state()) -> {reply, term(), state()} | {noreply, state()}.
+handle_call({call, Function, Args}, _From, St = #state{client = Client}) ->
+    {Result, ClientNext} = pm_client_api:call(?SERVICE, Function, Args, Client),
+    {reply, Result, St#state{client = ClientNext}};
+handle_call({pull_events, N, Timeout}, _From, St) ->
+    {Result, StNext} = poll_events(N, Timeout, St),
+    {reply, Result, StNext};
+handle_call({pull_history, BatchSize}, _From, St) ->
+    {Result, StNext} = poll_history(BatchSize, St),
+    {reply, Result, StNext};
+handle_call(Call, _From, State) ->
+    _ = logger:warning("unexpected call received: ~tp", [Call]),
+    {noreply, State}.
+
+-spec handle_cast(_, state()) -> {noreply, state()}.
+handle_cast(Cast, State) ->
+    _ = logger:warning("unexpected cast received: ~tp", [Cast]),
+    {noreply, State}.
+
+-spec handle_info(_, state()) -> {noreply, state()}.
+handle_info(Info, State) ->
+    _ = logger:warning("unexpected info received: ~tp", [Info]),
+    {noreply, State}.
+
+-spec terminate(Reason, state()) -> ok when Reason :: normal | shutdown | {shutdown, term()} | term().
+terminate(_Reason, _State) ->
+    ok.
+
+-spec code_change(Vsn | {down, Vsn}, state(), term()) -> {error, noimpl} when Vsn :: term().
+code_change(_OldVsn, _State, _Extra) ->
+    {error, noimpl}.
+
+%%
+
+poll_events(N, Timeout, St = #state{client = Client, poller = Poller}) ->
+    {Result, ClientNext, PollerNext} = pm_client_event_poller:poll(N, Timeout, Client, Poller),
+    {Result, St#state{client = ClientNext, poller = PollerNext}}.
+
+poll_history(BatchSize, St) ->
+    poll_history(BatchSize, [], St).
+
+poll_history(BatchSize, Acc, St) ->
+    case poll_events(BatchSize, 0, St) of
+        {Events, StNext} when length(Events) == BatchSize ->
+            poll_history(BatchSize, Acc ++ Events, StNext);
+        {Events, StNext} ->
+            {Acc ++ Events, StNext}
+    end.

commit 1ddc2a873573bcae74781e1577d49674063701fe

Fix get revision w/ empty index
Which was broken in 016faf5.

apps/party_management/src/pm_party_machine.erl
@@ -461,8 +461,7 @@ get_last_revision(PartyID) ->
 
 -spec get_last_revision_old_way(party_id()) -> party_revision() | no_return().
 get_last_revision_old_way(PartyID) ->
-    {History, Last, Step} = get_history_part(PartyID, undefined, ?STEP),
-    get_revision_of_part(PartyID, History, Last, Step).
+    get_revision_of_part(PartyID, undefined, ?STEP).
 
 -spec get_status(party_id()) -> party_status() | no_return().
 get_status(PartyID) ->
@@ -545,13 +544,13 @@ get_aux_state(PartyID) ->
             AuxState#{last_event_id => EventID}
     end.
 
-get_revision_of_part(PartyID, History, Last, Step) ->
+get_revision_of_part(PartyID, Last, Step) ->
+    {History, LastNext, StepNext} = get_history_part(PartyID, Last, Step),
     case find_revision_in_history(History) of
         revision_not_found when Last == 0 ->
             0;
         revision_not_found ->
-            {History1, Last1, Step1} = get_history_part(PartyID, Last, Step * 2),
-            get_revision_of_part(PartyID, History1, Last1, Step1);
+            get_revision_of_part(PartyID, LastNext, StepNext);
         Revision ->
             Revision
     end.
@@ -567,7 +566,7 @@ get_history_part(PartyID, Last, Step) ->
 
 find_revision_in_history([]) ->
     revision_not_found;
-find_revision_in_history([{_, _, ?party_ev(PartyChanges)} | Rest]) when is_list(PartyChanges) ->
+find_revision_in_history([{_, _, {PartyChanges, _}} | Rest]) when is_list(PartyChanges) ->
     case find_revision_in_changes(PartyChanges) of
         revision_not_found ->
             find_revision_in_history(Rest);

apps/party_management/test/pm_party_tests_SUITE.erl
@@ -31,6 +31,7 @@
 -export([complex_claim_acceptance/1]).
 
 -export([party_revisioning/1]).
+-export([party_get_initial_revision/1]).
 -export([party_get_revision/1]).
 -export([party_blocking/1]).
 -export([party_unblocking/1]).
@@ -154,6 +155,7 @@ groups() ->
         ]},
         {party_revisioning, [sequence], [
             party_creation,
+            party_get_initial_revision,
             party_revisioning,
             party_get_revision
         ]},
@@ -462,6 +464,7 @@ end_per_testcase(_Name, _C) ->
 -spec shop_update_before_confirm(config()) -> _ | no_return().
 -spec shop_update_with_bad_params(config()) -> _ | no_return().
 
+-spec party_get_initial_revision(config()) -> _ | no_return().
 -spec party_revisioning(config()) -> _ | no_return().
 -spec party_get_revision(config()) -> _ | no_return().
 
@@ -569,6 +572,12 @@ party_retrieval(C) ->
     Client = cfg(client, C),
     PartyID = cfg(party_id, C),
     #domain_Party{id = PartyID} = pm_client_party:get(Client).
+
+party_get_initial_revision(C) ->
+    % NOTE
+    % This triggers `pm_party_machine:get_last_revision_old_way/1` codepath.
+    Client = cfg(client, C),
+    0 = pm_client_party:get_revision(Client).
 
 party_revisioning(C) ->
     Client = cfg(client, C),