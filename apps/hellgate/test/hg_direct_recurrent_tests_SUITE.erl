2aecbb50b1adc84595bd2f1849dd260b80be5226
Yaroslav Rogov <rogovyaroslav@gmail.com>
2021-07-23 18:25:22 +0300

    ED-190/test: fix direct recurrent tests

--- apps/hellgate/test/hg_direct_recurrent_tests_SUITE.erl
+++ apps/hellgate/test/hg_direct_recurrent_tests_SUITE.erl
@@ -300,7 +300,8 @@ start_proxies(Proxies) ->
     ).
 
 setup_proxies(Proxies) ->
-    _ = hg_domain:upsert(Proxies).
+    _ = hg_domain:upsert(Proxies),
+    ok.
 
 start_service_handler(Module, C, HandlerOpts) ->
     start_service_handler(Module, Module, C, HandlerOpts).
