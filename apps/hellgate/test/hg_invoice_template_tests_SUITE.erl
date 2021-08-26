3c24a243587e0864cab1f6abe49d64e651a3b6c2
Yaroslav Rogov <rogovyaroslav@gmail.com>
2021-08-26 11:26:32 +0300

    ED-190/test: Fix compute_terms order

--- apps/hellgate/test/hg_invoice_template_tests_SUITE.erl
+++ apps/hellgate/test/hg_invoice_template_tests_SUITE.erl
@@ -457,10 +457,10 @@ terms_retrieval(C) ->
         }
     } = TermSet1,
     _ = hg_domain:update(construct_term_set_for_cost(5000, 11000)),
-    TermSet2 = hg_client_invoice_templating:compute_terms(TplID1, Timestamp, {timestamp, Timestamp}, Client),
 
     _ = timer:sleep(5000),
 
+    TermSet2 = hg_client_invoice_templating:compute_terms(TplID1, Timestamp, {timestamp, Timestamp}, Client),
     #domain_TermSet{
         payments = #domain_PaymentsServiceTerms{
             payment_methods =
